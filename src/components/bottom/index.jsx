81c7354f5764b053b4417c4021fcbb2d2866b257
cvaquedano <chvb2002@hotmail.com>
2019-03-17 08:51:47 -0600

    set style on bottomp section

--- src/components/bottom/index.jsx
+++ src/components/bottom/index.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import "./styke.scss"
 export default class BottomSection extends Component{
     constructor(props){
         super(props);
         this.state={};
     }
     render(){
-        return <div>Bottom Section</div>
+        return <div className="bottom-container">Bottom Section</div>
     }
 }
